5abb79b7bdedf40d3b399387dbae928966f28f84
Noob1701 <daniel.ansted@gmail.com>
2025-07-27 06:01:49 -0400

    Full Tests Passed Against Carlo

--- first_orchard_solver/gameplay/gamesims.py
+++ first_orchard_solver/gameplay/gamesims.py
@@ -14,8 +14,13 @@ class MultIterGame:
     random_results: List[int] = []
 
 
-def play_with_strat(strat: str) -> GameState:
+def play_with_strat(
+    strat: str, spaces: int = 5, full_inv: List[int] = [4, 4, 4, 4]
+) -> GameState:
     game_state = GameState()
+    game_state.raven_track.spaces = spaces
+    for key, new_value in zip(game_state.fruit_inventory.full_inv, full_inv):
+        game_state.fruit_inventory.full_inv[key] = new_value
     while not game_state.is_game_over():
         result = game_state.orchard_die.roll()
         strategies = {
@@ -34,12 +39,14 @@ def play_with_strat(strat: str) -> GameState:
     return game_state
 
 
-def run_strat_ntimes(n_runs: int, strat: str) -> GameResults:
+def run_strat_ntimes(
+    n_runs: int, strat: str, spaces: int = 5, full_inv: List[int] = [4, 4, 4, 4]
+) -> GameResults:
     game_results = GameResults()
     game_results.raven_end = 0
     game_results.fruit_end = 0
     for _ in range(n_runs):
-        game_state = play_with_strat(strat)
+        game_state = play_with_strat(strat, spaces, full_inv)
         if game_state.raven_track.spaces == 0:
             game_results.raven_end += 1
         else:
@@ -47,16 +54,18 @@ def run_strat_ntimes(n_runs: int, strat: str) -> GameResults:
     return game_results
 
 
-def run_batches(n_runs: int, n_times: int) -> MultIterGame:
+def run_batches(
+    n_runs: int, n_times: int, spaces: int = 5, full_inv: List[int] = [4, 4, 4, 4]
+) -> MultIterGame:
     mult_iter_game = MultIterGame()
     mult_iter_game.smallest_results.clear()
     mult_iter_game.largest_results.clear()
     mult_iter_game.random_results.clear()
     for _ in range(n_runs):
-        small_run = run_strat_ntimes(n_times, "smallest")
+        small_run = run_strat_ntimes(n_times, "smallest", spaces, full_inv)
         mult_iter_game.smallest_results.append(small_run.fruit_end)
-        large_run = run_strat_ntimes(n_times, "largest")
+        large_run = run_strat_ntimes(n_times, "largest", spaces, full_inv)
         mult_iter_game.largest_results.append(large_run.fruit_end)
-        random_run = run_strat_ntimes(n_times, "random")
+        random_run = run_strat_ntimes(n_times, "random", spaces, full_inv)
         mult_iter_game.random_results.append(random_run.fruit_end)
     return mult_iter_game
--- first_orchard_solver/tests/test_game_solver.py
+++ first_orchard_solver/tests/test_game_solver.py
@@ -1,5 +1,11 @@
+from collections import Counter
+from itertools import product
+
+import numpy as np
 import pytest
 
+from first_orchard_solver.gameplay.gamelogic import GameState
+from first_orchard_solver.gameplay.gamesims import run_batches
 from first_orchard_solver.gameplay.gamesolver import win_perc
 
 
@@ -16,7 +22,7 @@ from first_orchard_solver.gameplay.gamesolver import win_perc
         ((0, 0, 1, 0), 1, "random", 0.667),
         ((0, 0, 0, 2), 2, "random", 0.741),
     ],
-)
+)  # type: ignore[misc]
 def test_win_perc(
     full_inv: tuple[int, int, int, int], spaces: int, strat: str, expected: float
 ) -> None:
@@ -26,3 +32,53 @@ def test_win_perc(
     assert loss_prob + win_prob == pytest.approx(1, abs=0.02)
     "The win probability should be approximately 0.553."
     assert win_prob == pytest.approx(expected, abs=0.05)
+
+
+def test_win_perc_against_carlo() -> None:
+    """
+    This test is to ensure that the win_perc function is consistent with the
+    Monte Carlo method.It uses a known state and compares the output to
+    the expected value.
+    """
+    unique_states = set()
+    for full_inv_list in product(range(5), repeat=4):
+        multiple_states = tuple(sorted(Counter(full_inv_list).items()))
+        unique_states.add(multiple_states)
+    result = []
+    final_result = []
+    for state in unique_states:
+        sub_list = []
+        for value, count in state:
+            sub_list.extend([value] * count)
+        result.append(sub_list)
+    for sublist in result:
+        for i in range(1, 6):
+            final_result.append(sublist + [i])
+    for item in final_result:
+        assert len(item) == 5, f"Each item should have 5 elements.{len(item)} found."
+        assert sum(item[:4]) <= 16
+        assert sum(item[:4]) >= 0
+        assert sum(item[4:]) <= 5
+    assert len(final_result) == 350
+    game_state = GameState()
+    for scenario in final_result:
+        game_state.fruit_inventory.full_inv = {
+            3: scenario[0],
+            4: scenario[1],
+            5: scenario[2],
+            6: scenario[3],
+        }
+        game_state.raven_track.spaces = scenario[4]
+        batches = run_batches(2000, 100, scenario[4], scenario[:4])
+        solved_small = win_perc(tuple(scenario[:4]), scenario[4], "small")
+        solved_large = win_perc(tuple(scenario[:4]), scenario[4], "large")
+        solved_random = win_perc(tuple(scenario[:4]), scenario[4], "random")
+        assert np.mean(batches.smallest_results) / 100 == pytest.approx(
+            solved_small[0], abs=0.05
+        )
+        assert np.mean(batches.largest_results) / 100 == pytest.approx(
+            solved_large[0], abs=0.05
+        )
+        assert np.mean(batches.random_results) / 100 == pytest.approx(
+            solved_random[0], abs=0.05
+        )
